8606e38174b2b12304b5d01e2b5ebbc2826f734f
Rohit Parab <81211724+Azazel990@users.noreply.github.com>
2021-04-28 21:42:29 +0530

    Add files via upload

---
+++ C++/Square root Log(n).cpp
@@ -0,0 +1,45 @@
+//square_root without using in-built functions
+// Time Complexity  = O(Log(n))
+//Space Complexity = O(1)    
+#include<iostream>
+using namespace std;
+
+class solution
+{
+    public : 
+        int square_root(int n)
+        {
+            int l=1,r=n,mid;
+            
+            while(l<=r)
+            {
+                mid = (l+r-1)/2;
+                if(mid*mid==n)
+                {
+                    return mid;
+                }
+                else if(mid*mid > n)
+                {
+                    r = mid-1;
+                }
+                else if(mid*mid < n)
+                {
+                    l = mid + 1;
+                    if(r==mid + 1)
+                    {
+                        return mid;
+                    }
+                }
+            }
+        }
+};
+
+int main()
+{
+    solution s;
+    int n=36;
+    
+    cout<<"Ans = "<<s.square_root(n);
+
+}
+
