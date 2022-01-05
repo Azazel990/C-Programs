c3fba7d164107ce9c64cb59c50a3683c09a13ed0
Rohit Parab <81211724+Azazel990@users.noreply.github.com>
2022-01-05 13:40:23 +0530

    Update tree.cpp

--- C++/tree.cpp
+++ C++/tree.cpp
@@ -13,7 +13,7 @@ struct Node
         left = NULL;
         right = NULL;
     }
-}*root;
+}*root,*last;
  
 
 void InsertNode(int num)
@@ -21,6 +21,7 @@ void InsertNode(int num)
     if(root==NULL)
     {
        root = new Node(num);
+       last = root;
     }
     else
     {
@@ -34,12 +35,14 @@ void InsertNode(int num)
             if(temp->left==NULL)
             {
                temp->left =  new Node(num);
+               last = temp->left;
                 return;
             }
             else q.push(temp->left);
             if(temp->right==NULL)
             {
                 temp->right = new Node(num);
+                last = temp->right;
                 return;
             }
             else q.push(temp->right);
@@ -48,6 +51,58 @@ void InsertNode(int num)
     }
 }
        
+void Delete(int num)
+{
+    queue<Node *> q;
+    q.push(root);
+    
+    while(!q.empty())
+    {
+        Node *temp = q.front();
+        
+        if(temp->data==num)
+        {
+            if(temp->left==NULL&&temp->right==NULL) 
+            {
+                temp==NULL;
+                return;
+            }
+            else
+            {
+                temp->data = last->data;
+                while(!q.empty()) q.pop();
+                
+                q.push(root);
+                while(!q.empty())
+                {
+                    Node *temp1 = q.front();
+                    if(temp1->left!=NULL) 
+                    {
+                        if(temp1->left==last)
+                        {
+                            last = NULL;
+                            last = temp1->left;
+                            return;
+                        }
+                        else q.push(temp1->left);
+                    }
+                    if(temp1->right!=NULL)
+                    {
+                        if(temp1->right==last)
+                        {
+                            last = NULL;
+                            last = temp1->right;
+                            return;
+                        }
+                        else q.push(temp1->right);
+                    }
+                    q.pop();
+                }
+            }
+        }
+        
+    }
+}
 void Preorder(struct Node* root)
 {
     if(root==NULL) return;
@@ -75,6 +130,54 @@ void Postorder(struct Node *root)
     Postorder(root->right);
     cout<<root->data<<" ";
 }
+int LevelOrder(struct Node *root,int flag)
+{
+    int count = 0;
+    if(root==NULL) return count;
+    
+    queue<Node *> q;
+    q.push(root);
+    count++;
+    while(!q.empty())
+    {
+        Node *temp = q.front();
+        if(flag==1) cout<<temp->data<<" ";
+        q.pop();
+        
+        if(temp->left!=NULL) 
+        {
+            q.push(temp->left);
+            count++;
+        }
+        if(temp->right!=NULL){
+            q.push(temp->right);
+            count++;
+        }
+        
+    }
+    return count;
+}
+
+int LeafNodes(struct Node* root)
+{
+    if(root==NULL) return 0;
+    
+    if(root->left==NULL&&root->right==NULL) return 1;
+    
+    return LeafNodes(root->left) + LeafNodes(root->right);
+}
+
+int Height(struct Node * root)
+{
+    if(root==NULL) return 0;
+    
+    if(root->left==NULL&&root->right==NULL) return 0;
+    
+    int l = Height(root->left);
+    int r = Height(root->right);
+    
+    return 1 + max(l,r);
+}
 
 int main()
 {
@@ -83,7 +186,7 @@ int main()
     
     while(1)
     {
-        cout<<"Please Enter Your Choice... \n1.Insert new node\n2.Preorder Traversal\n3.Inorder Traversal\n4.Postorder Traversal\n";
+        cout<<"Please Enter Your Choice... \n1.Insert new node\n2.Delete Node\n3.Preorder Traversal\n4.Inorder Traversal\n5.Postorder Traversal\n6.Levelorder Traversal\n7.Count Nodes\n8.Count Leaf Nodes\n9.Count Non-Leaf Nodes\n10.Height of the tree\n";
         cin>>choice;
         switch(choice)
         {
@@ -91,15 +194,30 @@ int main()
                      cin>>num;
                      InsertNode(num);
                      break;
-            case 2 : Preorder(root);
+            case 2 : cout<<"Please Enter the data to be deleted : ";
+                     cin>>num;
+                     Delete(num);
+                     break;
+            case 3 : Preorder(root);
                       cout<<endl;
                       break;
-            case 3 : Inorder(root);
+            case 4 : Inorder(root);
                      cout<<endl;
                      break;
-            case 4 : Postorder(root);
+            case 5 : Postorder(root);
                      cout<<endl;
                      break;
+            case 6 : LevelOrder(root,1);
+                     cout<<endl;
+                     break;
+            case 7 : cout<<LevelOrder(root,0)<<"\n";
+                     break;
+            case 8 : cout<<LeafNodes(root)<<"\n";
+                     break;
+            case 9 : cout<<LevelOrder(root,0) - LeafNodes(root)<<"\n";
+                     break;
+            case 10 : cout<<Height(root)<<"\n";
+                      break;
             default : exit(0);
         }
     }
